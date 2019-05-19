40544ee0db1d5c93e0e95588b7fbc275765c0cc3
zpeng1989 <592392714@qq.com>
2019-05-20 05:37:30 +0800

    first part

--- tensorflow-学习/007_tensorflow_keras.py
+++ tensorflow-学习/007_tensorflow_keras.py
@@ -124,11 +124,127 @@ def find_next_odd():
 print(find_next_odd())
 print(v)
 
-def perpare_mnist_features_and_labels(x,y):
+def prepare_mnist_features_and_labels(x,y):
     x = tf.cast(x, tf.float32)/255
     y = tf.cast(y, tf.int64)
     return x,y
+'''
+
+def prepare_mnist_features_and_labels(x, y):
+    x = tf.cast(x, tf.float32) / 255.0
+    y = tf.cast(y, tf.int64)
+    return x, y
+
+'''
+
+
+def mnist_dataset():
+    (x,y), _ = tf.keras.datasets.mnist.load_data()
+    ds = tf.data.Dataset.from_tensor_slices((x,y))
+    ds = ds.map(prepare_mnist_features_and_labels)
+    #ds = ds.map(prepare_mnist_features_and_labels)
+    ds = ds.take(20000).shuffle(20000).batch(100)
+    return ds
+'''
+
+def mnist_dataset():
+    (x, y), _ = tf.keras.datasets.mnist.load_data()
+    ds = tf.data.Dataset.from_tensor_slices((x, y))
+    ds = ds.map(prepare_mnist_features_and_labels)
+    ds = ds.take(20000).shuffle(20000).batch(100)
+    return ds
+'''
+
+train_dataset = mnist_dataset()
+model = tf.keras.Sequential((
+    tf.keras.layers.Reshape(target_shape = (28 * 28,), input_shape = (28, 28)),
+    tf.keras.layers.Dense(100, activation = 'relu'),
+    tf.keras.layers.Dense(100, activation = 'relu'),
+    tf.keras.layers.Dense(10)))
+
+model.build()
+print(model.summary())
+optimizer = tf.keras.optimizers.Adam()
+compute_loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits = True)
+
+compute_accuracy = tf.keras.metrics.SparseCategoricalAccuracy()
+
+'''
+
+train_dataset = mnist_dataset()
+model = tf.keras.Sequential((
+    tf.keras.layers.Reshape(target_shape=(28 * 28,), input_shape=(28, 28)),
+    tf.keras.layers.Dense(100, activation='relu'),
+    tf.keras.layers.Dense(100, activation='relu'),
+    tf.keras.layers.Dense(10)))
+model.build()
+optimizer = tf.keras.optimizers.Adam()
+compute_loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+compute_accuracy = tf.keras.metrics.SparseCategoricalAccuracy()
+
+'''
+
+def train_one_step(model, optimizer, x, y):
+    with tf.GradientTape() as tape:
+        logits = model(x)
+        loss = compute_loss(y, logits)
+    grads = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(grads, model.trainable_variables))
+    compute_accuracy(y, logits)
+    return loss
+
+
+'''
+def train_one_step(model, optimizer, x, y):
+    with tf.GradientTape() as tape:
+        logits = model(x)
+        loss = compute_loss(y, logits)
+
+    grads = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+    compute_accuracy(y, logits)
+    return loss
+'''
+
+
+@tf.function
+def train(model, optimizer):
+    train_ds = mnist_dataset()
+    step = 0
+    loss = 0.0
+    accuracy = 0.0
+    for x, y in train_ds:
+        step += 1
+        loss = train_one_step(model, optimizer, x, y)
+        if tf.equal(step % 10, 0):
+            tf.print('Step', step, ': loss', loss, '; accuracy',compute_accuracy.result())
+    return step, loss, accuracy
+    
+step, loss, accuracy = train(model, optimizer)
+print('Final step', step,':loss', loss, '; accuracy', compute_accuracy.result())
 
 
 
+'''
+@tf.function
+def train(model, optimizer):
+    train_ds = mnist_dataset()
+    step = 0
+    loss = 0.0
+    accuracy = 0.0
+    for x, y in train_ds:
+        step += 1
+        loss = train_one_step(model, optimizer, x, y)
+        if tf.equal(step % 10, 0):
+            tf.print('Step', step, ': loss', loss,
+               '; accuracy', compute_accuracy.result())
+    return step, loss, accuracy
 
+
+step, loss, accuracy = train(model, optimizer)
+print('Final step', step, ': loss', loss,
+      '; accuracy', compute_accuracy.result())
+
+'''
