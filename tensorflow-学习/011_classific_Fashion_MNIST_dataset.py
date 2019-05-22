2617a0ef4b892bb7feb8e469c8a9d87402e4b5fe
zpeng1989 <592392714@qq.com>
2019-05-22 08:20:28 +0800

    add tensorflow

---
+++ tensorflow-学习/011_classific_Fashion_MNIST_dataset.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+import tensorflow.keras as keras
+import numpy as np
+
+(train_images, train_labels), (test_images, test_labels) = keras.datasets.fashion_mnist.load_data()
+
+class_names = ['T-shirt/top','Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+print(train_images.shape)
+print(train_labels.shape)
+print(test_images.shape)
+print(test_labels.shape)
+
+
+
+
