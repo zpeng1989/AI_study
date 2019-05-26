337bfdfe75aaa3f2fe9f96f6974d7be7f46fb1b2
zpeng1989 <592392714@qq.com>
2019-05-26 16:46:02 +0800

    overfit-v3

--- tensorflow-学习/013_overfitting_method.py
+++ tensorflow-学习/013_overfitting_method.py
@@ -20,3 +20,17 @@ def multi_hot_sequences(sequences, dimension):
 train_data = multi_hot_sequences(train_data, dimension = NUM_WORDS)
 test_data = multi_hot_sequences(test_data,dimension = NUM_WORDS)
 
+import tensorflow.keras.layers as layers
+
+baseline_model = keras.Sequential(
+    [
+        layers.Dense(16, activation = 'relu', input_shape = (NUM_WORDS,)),
+        layers.Dense(16, activation = 'relu'),
+        layers.Dense(1, activation = 'sigmoid')
+    ]
+)
+
+baseline_model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy', 'binary_crossentropy'])
+print(baseline_model.summary())
+
+baseline_history = baseline_model.fit(train_data,train_labels,epochs = 20, batch_size = 512, validation_data = (test_data,test_labels),verbose = 1)
