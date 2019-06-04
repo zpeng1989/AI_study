0c0935b03bfec505af6c3cecb6af18d7d14d724c
zpeng1989 <592392714@qq.com>
2019-06-04 20:44:45 +0800

    encode

---
+++ tensorflow-学习/027_Deconde_net.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*
+
+import tensorflow as tf
+import tensorflow.keras as keras
+import tensorflow.keras.layers as layers
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+x_train = x_train.reshape((-1, 28*28))/255.0
+x_test = x_test.reshape((-1, 28*28))/255.0
+
+#print(x_train[1])
+
+print(x_train.shape)
+
+code_dim = 32
+
+inputs = layers.Input(shape = (x_train.shape[1],), name= 'inputs')
+code = layers.Dense(code_dim, activation = 'relu', name = 'code')(inputs)
+outputs = layers.Dense(x_train.shape[1], activation = 'softmax', name = 'outps')(code)
+
+auto_encoder = keras.Model(inputs, outputs)
+
+print(auto_encoder.summary())
+
+
