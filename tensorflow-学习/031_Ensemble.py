b412614aaac57e844c886e5dd14233f664cae3aa
zpeng1989 <592392714@qq.com>
2019-06-05 22:09:40 +0800

    embed

--- tensorflow-学习/031_Ensemble.py
+++ tensorflow-学习/031_Ensemble.py
@@ -8,3 +8,19 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
+
+
+
+vcoab_size = 10000
+(train_x, train_y), (test_x, test_y) = keras.datasets.imdb,load_data(num_words = vocab_size)
+print(train_x[0])
+
+word_index = keras.datasets.imdb.get_word_index()
+word_index = {k:(v+3) for k,v in word_index.items()}
+word_index['<PAD>'] = 0
+word_index['<START>'] = 1
+word_index['<UNK>'] = 2
+word_index['<UNUSED>'] = 3
+reverse_word_index = {v:k for k, v in word_index.items()}
+
+
