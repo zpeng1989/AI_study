ab52cf44a10074738aa80c40d724fa19c938806e
zpeng1989 <592392714@qq.com>
2019-06-07 10:50:14 +0800

    sss

--- tensorflow-学习/032_DCGAN.py
+++ tensorflow-学习/032_DCGAN.py
@@ -8,4 +8,28 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
+(train_image, train_labels),(_,_) = tf.keras.datasets.mnist.load_data()
+train_image = train_image.reshape(train_image.shape[0], 28, 28, 1).astype('float32')
+train_image= （train_images- 127.5）/127.5
+
+BUFFER_SIZE = 60000
+BATCH_SIZE = 256
+
+train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+
+def make_generator_model():
+    model = tf.keras.Sequential()
+    model.add(layers.Dense(7*7*256, use_bias = False, input_shape = (100,)))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+    model.add(layers.Reshape((7,7, 256)))
+    assert model.output_shape == (None, 7, 7, 256)
+    model.add(layers.Conv2DTranspose(128, (5,5), strides = (1,1), padding = 'same', use_bias = False))
+    assert mdoel.output_shape == (None, 7, 7, 128)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+    model.add(layers.Conv2DTranspose(64, (5,5), strides = (2,2), padding = 'same', use_bias = False))
+    
+
+
 
