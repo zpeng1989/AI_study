ef383cb8b5af68a6d30dfdfa9fe705696eaf6183
zpeng1989 <592392714@qq.com>
2019-06-13 21:26:29 +0800

    GBT

---
+++ tensorflow-学习/033_GBT.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import, division, print_function
+
+import numpy as np
+import pandas as pd
+from IPython.display import clear_output
+
+# Load dataset.
+dftrain = pd.read_csv(
+    'https://storage.googleapis.com/tf-datasets/titanic/train.csv')
+dfeval = pd.read_csv(
+    'https://storage.googleapis.com/tf-datasets/titanic/eval.csv')
+
+print(dftrain.head())
+y_train = dftrain.pop('survived')
+y_eval = dfeval.pop('survived')
+
+import tensorflow as tf
+
+tf.random.set_seed(123)
+
+print(dftrain.describe())
+
+
+
+
