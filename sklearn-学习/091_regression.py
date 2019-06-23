e5b8914123c55a938c5e54bd58b713172fd63ef5
zpeng1989 <592392714@qq.com>
2019-06-23 18:42:07 +0800

    regressage

---
+++ sklearn-学习/091_regression.py
@@ -0,0 +1,12 @@
+from sklearn.linear_model import LinearRegression as LR
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.satasets import fetch_california_housing as fch
+import pandas as pd
+
+
+housevalue = fch()
+
+
+
+
