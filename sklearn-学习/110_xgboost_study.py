4f39165f7ce58cd4c70c82fde8f64718fbbc4355
zpeng1989 <592392714@qq.com>
2019-06-25 22:16:00 +0800

    add XGB

---
+++ sklearn-学习/110_xgboost_study.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import xgboost as xgb
+
+from xgboost import XGBRegressor as XGBR
+from sklearn.ensemble import RandomForestRegressor as RFR
+from sklearn.linear_model import LinearRegression as LinearR
+from sklearn.datasets import load_boston
+from sklearn.model_selection import KFold, cross_val_score as CVS, train_test_split as TTS
+from sklearn.metrics import mean_squared_error as MSE
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import time import time
+import datetime
+
+
+data = load_boston()
+
+X = data.data
+y = data.target
