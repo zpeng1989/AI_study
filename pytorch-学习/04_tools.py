0dcfcaf6587cf28ea95840c87fedee6a72f44bb8
zpeng1989 <592392714@qq.com>
2019-07-04 18:13:41 +0800

    add data

---
+++ pytorch-学习/04_tools.py
@@ -0,0 +1,26 @@
+import torch as t
+from torch.utils import data
+
+import os
+from PIL import Image
+import numpy as np
+
+class DogCat(data.Dataset):
+    def __init__(self, root):
+        imgs = os.listdir(root)
+        self.imgs = [os.path.join(root, img) for img in imgs]
+
+    def __getitem__(self, index):
+        img_path = self.imgs[index]
+        label = 1 if 'dog' in img_path.split('/')[-1] else 0
+        pil_img = Image.open(img_path)
+        array = np.asarray(pil_img)
+        data = t.from_numpy(array)
+        return data, label
+    def __len__(self):
+        return len(self.imgs)
+
+
+dataset = DogCat('./data/dogcat/')
+
+
