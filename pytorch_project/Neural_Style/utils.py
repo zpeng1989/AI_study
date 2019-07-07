adf960a14b357f2b553a3e33c254f73c960be1af
zpeng1989 <592392714@qq.com>
2019-07-07 21:27:14 +0800

    add netural style

--- pytorch_project/Neural_Style/utils.py
+++ pytorch_project/Neural_Style/utils.py
@@ -27,6 +27,40 @@ class Visualizer():
         self.vis = visdom.Visdom(env = env, use_incoming_socket = False, **kwargs)
         return self
 
-        
+    def plot_many(self, name, y):
+        x = self.index.get(name, 0)
+        self.vis.line(Y = np.array([y]),
+                      X = np.array([x]),
+                      win = name,
+                      opts = dict(title = name),
+                      update = None if x == 0 else 'append'
+                      )
+        self.index[name] = x + 1
+
+    def plot(self, name, y):
+        x = self.index.get(name, 0)
+        self.vis.line(Y = np.array([y]),
+                      X = np.array([x]),
+                      win = name,
+                      opts = dict(title = name)
+        )
+    
+    def img_grid_many(self, d):
+        for k, v in d.items():
+            self.img_grid(k,v)
+
+    def img_grid(self, name, input_3d):
+        self.img(name, tv.utils.make_grid(input_3d.cpu()[0].unsqueeze(1).clamp(max =1, min = 0)))
+
+    def log(self, info, win = 'log_text'):
+        self.log_text += ('[{time}] {info} <br>'.format(
+            time=time.strftime('%m%d_%H%M%S'),
+            info=info))
+        self __getattr__(self, name):
+            return getattr(self.vis, name)
+
+
+
+
 
 
