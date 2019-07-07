057cb21dbec59c71bc998e1291b683a89c00986c
zpeng1989 <592392714@qq.com>
2019-07-07 22:18:27 +0800

    vgg16

--- pytorch_project/Neural_Style/PackedVGG.py
+++ pytorch_project/Neural_Style/PackedVGG.py
@@ -6,4 +6,17 @@ from collections import namedtuple
 
 class Vgg16(torch.nn.Module):
     def __init__(self):
-        super(Vgg16)
+        super(Vgg16,self).__init__()
+        features = list(vgg16(pretrained = True).features)[:23]
+        self.features = nn.ModuleList(features).eval()
+
+        
+    def forward(self, x):
+        reaults = []
+        for ii, model in enumerate(self.features):
+            x = model(x)
+            if ii in {3, 8, 15, 22}:
+                results.append(x)
+        vgg_outputs = namedtuple(
+            'VggOutputs', ['relu1_2', 'relu2_2', 'relu3_3', 'relu4_3'])
+        return vgg_outputs(*reaults)
