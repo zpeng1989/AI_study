71db6e4e40565e7c98215ef184ad00723534da20
zpeng1989 <592392714@qq.com>
2019-07-08 20:24:28 +0800

    neturl sytle

--- pytorch_project/Neural_Style/main.py
+++ pytorch_project/Neural_Style/main.py
@@ -45,6 +45,54 @@ def train(**kwargs):
         setattr(opt, k_, v_)
     device = t.device('cuda') if opt.use_gpu else t.device('cpu')
     vis = utils.Visualizer(opt.env)
+    transfroms = tv.transforms.Compose([
+            tv.transforms.Resize(opt.image_size),
+            tv.transforms.CenterCrop(opt.image_size),
+            tv.transforms.ToTensor(),
+            tv.transforms.Lambda(lambda x: x * 255)
+        ])
+    dataset = tv.datasets.ImageFolder(opt.data_root, transfroms)
+    dataloader = data.DataLoader(dataset, opt.batch_size)
+
+    transformer = TransformerNet()
+    if opt.model_path:
+        transformer.load_state_dict(t.load(opt.model_path, map_location =lambda _s, _: _s))
+    transformer.to(device)
+
+    vgg = Vgg16().eval()
+    vgg.to(device)
+    for param in vgg.parameters():
+        param.requires_grad = False
+
+    optimizer = t.optim.Adam(transformer.parameters(), opt.lt)
+
+    style = style.to(device)
+    with t.no_grad():
+        features_style = vgg(style)
+        gram_style = [utils.gram_matrix(y) for y in features_style]
+
+
+    style_meter = tnt.meter.AverageValueMeter()
+    content_meter = tnt.meter.AverageValueMeter()
+
+    for epoch in range(opt.epoches):
+        content_meter.reset()
+        style_meter.reset()
+        for epoch in range(opt.epoches):
+            content_meter.reset()
+            style_meter.reset()
+            for ii, (x,  _) in tqdm.tqdm(enumerate(dataloader)):
+                optimizer.zero_grad()
+                x = x.to(device)
+                y = transformer(x)
+                y = utils.normalize_batch(y)
+                x = utils.normalize_batch(x)
+                features_y = vgg(y)
+                features_x = vgg(x)
+
+                content_loss = opt.content_weight * F.mse_loss(features_y.relu2_2, features_x.relu2_2)
+                style_loss = 0.
+
 
 
 
