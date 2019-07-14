14996b74f2df998958455544b96625635e0ddbf3
zpeng1989 <592392714@qq.com>
2019-07-14 19:24:41 +0800

    add new 3

--- tensorflow-学习/051_tensorflow_edger.py
+++ tensorflow-学习/051_tensorflow_edger.py
@@ -5,3 +5,10 @@ import tensorflow as tf
 print(tf.add(1,2))
 print(tf.add([3.8],[2,5]))
 
+print(tf.square(6))
+print(tf.reduce_sum([7,8,9]))
+print(tf.square(3) + tf.square(4))
+
+
+tf.Tensor(3, shape = (), stype = int32)
+tf.Tensor([5 13], shape = (2,),dtype = int32)
