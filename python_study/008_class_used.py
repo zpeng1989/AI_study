cbaf248ec3e56a3ad91888792a852ddce98d8f60
zpeng1989 <592392714@qq.com>
2019-07-21 21:46:51 +0800

    add class

--- python_study/008_class_used.py
+++ python_study/008_class_used.py
@@ -1,1 +1,25 @@
 class Student(object):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def study(self, course_name):
+        print('At age at %s with study by %s'%(self.name, course_name))
+
+    def watch_movie(self):
+        if self.age < 18:
+            print('%s only watch.'% self.name)
+        else:
+            print('%s avv' % self.name)
+
+
+
+def main():
+    stu1 = Student('aa', 38)
+    stu1.study('python class')
+    stu1.watch_movie()
+    stu2 = Student('bb', 15)
+    stu2.study('sixiang')
+    stu2.watch_movie()
+
+if __name__ == '__main__':
+    main()
