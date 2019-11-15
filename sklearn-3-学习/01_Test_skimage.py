b52ee1a79dbdbed0b5a04845a04e8658067d9223
root <root@system-ai.origimed.com>
2019-11-15 16:36:09 +0800

    up s

--- sklearn-3-学习/01_Test_skimage.py
+++ sklearn-3-学习/01_Test_skimage.py
@@ -7,7 +7,7 @@ import os
 #from matplotlib import pyplot as plot
 import skimage
 from skimage import io, transform, color, img_as_ubyte, img_as_float
-
+import numpy as np
 
 #img_file1 = img.open('2.png')
 img_file2 = io.imread('2.png')
@@ -82,6 +82,136 @@ io.imsave('p_3.png',pic_thr)
 #io.imsave('p_4.png',pic_for)
 
 print(type(img_file2))
+
+### -----------------
+### 修改大小 resize()
+### -----------------
+### skimage.transform.resize(image, output_shape)
+
+from skimage import transform, data
+img = data.camera()
+dst = transform.resize(img, (80, 60))
+
+### ----------------
+### 按照比例修改 rescale()
+### ----------------
+
+print(img.shape)
+print(transform.rescale(img, 0.1).shape)
+print(transform.rescale(img, [0.5, 0.25]).shape)
+print(transform.rescale(img, 2).shape)
+
+### -----------------
+### 旋转 rotate
+### -----------------
+
+
+from skimage import transform, data
+
+img = data.camera()
+print(img.shape)
+
+img1 = transform.rotate(img, 60)
+print(img1.shape)
+
+img2 = transform.rotate(img, 30, resize = True)
+print(img2.shape)
+
+
+#####################
+# 对比对增强 exposure
+#####################
+# skimage.exposure.adjust_gamma(image, gamma = 1)
+
+from skimage import data, exposure, img_as_float
+image = img_as_float(data.moon())
+gam1 = exposure.adjust_gamma(image, 2)
+gam2 = exposure.adjust_gamma(image, 0.5)
+
+io.imsave('out1.png', gam1)
+io.imsave('out1_2.png', gam2)
+
+from skimage import data, exposure
+image = data.moon()
+result = exposure.is_low_contrast(image)
+
+print(result)
+
+####################
+# 拉伸 skimage.exposure.rescale_intensity(image, in_range = 'image', out_range = 'dtype')
+###################
+image = np.array([51, 102, 153], dtype = np.uint8)
+mat = exposure.rescale_intensity(image)
+print(mat)
+
+tmp = image * 1.0
+mat = exposure.rescale_intensity(tmp)
+print(mat)
+
+
+image = np.array([-10, 0, 10], dtype = np.int8)
+mat = exposure.rescale_intensity(image, out_range = (0, 127))
+print(mat)
+
+
+
+#####################
+# 绘制直方图 skimage.exposure.histogram(image, nbins = 256)
+#####################
+
+image = data.camera()*1.0
+
+hist1 = np.histogram(image, bins = 2)
+
+print(hist1)
+#io.imsave('his.png', hist1)
+
+import matplotlib.pyplot as plt
+
+img=data.camera()
+arr=img.flatten()
+print('KKKKKKKKKKKKKKKKKKKKK')
+print(arr)
+print('IIIIIIIIIIIIIIIIIIIII')
+print(arr.min())
+print(arr.max())
+print(arr.mean())
+
+#n, bins, patches = plt.hist(arr, bins=256, normed=1,edgecolor='None',facecolor='red') 
+#print(n)
+#print(bins)
+#print(patches)
+
+
+
+img = io.imread('2.png')
+ar=img[:,:,0].flatten()
+ag=img[:,:,1].flatten()
+ab=img[:,:,2].flatten()
+
+print(ar.mean())
+print(ag.mean())
+print(ab.mean())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
